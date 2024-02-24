ea4c393eca545697060a2a073c9b32676c99a818
Vinayak Gupta <vinayakgupta.13.6@gmail.com>
2024-02-24 17:25:55 +0530

    write doc string for read image method

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def read_image(image_path, show_image=False):
+    """
+    Reads an image from the given path and returns it as a numpy array. If the `show_image` parameter is set to True, the function will also display the image in a new window.
+
+    Args:
+        image_path (str): A string representing the path to the image file.
+        show_image (bool, optional): A boolean indicating whether to display the image in a new window. Defaults to False.
+
+    Returns:
+        numpy.ndarray: A numpy array representing the image.
+
+    Raises:
+        Exception: An exception is raised if the image file is not found or if there is an error loading the image.
+
+    Example:
+        import cv2
+        import logging
+
+        # Set up logging
+        logging.basicConfig(level=logging.INFO)
+
+        # Read an image from file
+        img = read_image("path/to/image.jpg")
+
+        # Display the image
+        read_image("path/to/image.jpg", show_image=True)
+    """
     try:
         # Loading image from the given path
         img = cv2.imread(image_path)
@@ -24,9 +50,8 @@ def read_image(image_path, show_image=False):
             
     except Exception as e:
         print(f"Check your image file present in {image_path}. It gave the following error \n\t{e}")
-    
 
-    
+
 
 def main(image_path):
     # Read the image
